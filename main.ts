83cf20373020546bc4e470c824b6416af15ae5b1
eric645 <37544618+eric645@users.noreply.github.com>
2018-11-01 15:19:06 -0400

    added comment

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% width="100" color=#0000ff icon=" "
+namespace comment {
+    //% block="// %g"
+    //% advanced=true
+    //% js="// %g"
+    function comment(g: string) { }
+}
